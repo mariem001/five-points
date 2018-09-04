982d0c8278c4aaca593a5bc2fd584d2791de4fa6
Mariem Amami <mariem.amami@gmail.com>
2018-09-04 20:30:59 +0100

    Add routes

--- app.js
+++ app.js
@@ -2,10 +2,23 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+app.get('/',(req, res, next) => {
     res.status(200).json({
         message:"Hello World!"
     });
 });
 
+
+app.route('/book')
+  .get(function(req, res) {
+    res.send('Get a random book');
+  })
+  .post(function(req, res) {
+    res.send('Add a book');
+  })
+  .put(function(req, res) {
+    res.send('Update the book');
+  });
+
+  
 module.exports = app;
